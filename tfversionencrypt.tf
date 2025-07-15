0897dbab9c3918e1877564449ac875d8178db6f6
Yahshua Emmanuel George Ball <130876430+lonewolfxiaolin@users.noreply.github.com>
2025-07-15 13:34:04 -0400

    Update tfversionencrypt.tf

--- tfversionencrypt.tf
+++ tfversionencrypt.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "terraform-state-dragon-hadou-fu"
+  bucket        = "terraform-state-dragon-hadou"
   force_destroy = true
 }
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
